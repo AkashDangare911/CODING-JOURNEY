d7d9f95ffae8d2cf8d91e56d4c0459d5b294de95
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2024-09-22 00:46:55 +0530

    Time: 3 ms (66.94%), Space: 49 MB (14.07%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> ans = new LinkedList<>();
+        for(int j=1; j<=9;j++){
+            if(j<=n) fun(j, n, ans);
+        }
+        
+    
+        return ans;
+
+    }
+
+    static void fun(int i, int n,  List<Integer> ans){
+        ans.add(i);
+
+        for (int j = 0; j <= 9 ; j++) {
+            int num = i * 10 + j;
+            if(num<=n) fun(num, n, ans);
+        }
+    }
+}
