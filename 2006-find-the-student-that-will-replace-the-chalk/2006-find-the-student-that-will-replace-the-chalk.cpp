e31d816b46d1ed3385b23c2c7becff9f915079f1
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2024-09-25 01:02:11 +0530

    Time: 97 ms (47.94%), Space: 77.1 MB (17.24%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -3,25 +3,24 @@ public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int ans = -1;
         int n=chalk.size();
-        vector<int> prefixSum(n,0);
-        prefixSum[0]=chalk[0];
 
-        if(prefixSum[0] > k){
+        if(chalk[0] > k){
             return 0;
         }
+
         // find pref sum and if k exhaust, then  return ans index
        for(int i=1;i<n;i++){
-        prefixSum[i]=prefixSum[i-1]+chalk[i];
-        if(prefixSum[i] > k){
+        chalk[i]=chalk[i-1]+chalk[i];
+        if(chalk[i] > k){
             return i;
         }
        }
 
         // get count of final rotation
-        int remain = k%prefixSum[n-1];
+        int remain = k%chalk[n-1];
 
         for(int i=0;i<n;i++){
-            if(remain < prefixSum[i]){
+            if(remain < chalk[i]){
                 return i;
             }
         }
