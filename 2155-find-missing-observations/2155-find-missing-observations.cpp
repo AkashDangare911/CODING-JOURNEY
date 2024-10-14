2ddd81d32983ee018c6d01c283016c0753639db0
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2024-10-15 01:10:52 +0530

    Time: 96 ms (60.03%), Space: 116.3 MB (60.24%) - LeetHub

--- 2155-find-missing-observations/2155-find-missing-observations.cpp
+++ 2155-find-missing-observations/2155-find-missing-observations.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
-        vector<int> ans;
+        // vector<int> ans;
         int m = rolls.size();
         int total = (m+n)*mean;
         int sum = accumulate(rolls.begin(),rolls.end(),0);
         int remain = total-sum;
         
-        int each = remain/n;
-        // cout<<"each"<<each<<endl;
-        // cout<<"total"<<total<<endl;
-        // cout<<"remain"<<remain<<endl;
-        // cout<<"sum"<<sum<<endl;
-
+        // unsatisfied conditions
         if(n>remain || remain>n*6 || sum>total)  return {};
 
+        // divide remained equally, then distribute the remained
+        int each = remain/n;
+        vector<int> ans(n,each);
         int extra = remain - (each*n);
-        // if(extra && each==6){
-        //     return {};
-        // }
+        int i=0;
 
-        n-=extra;
-        each++;
-        
         while(extra--){
-            ans.push_back(each);
-            remain-=each;
-        }
-        each--;
-
-        while(--n)
-        {
-            ans.push_back(each);
-            remain-=each;
-        }
-
-        // cout<<"greattt\n";
-        // cout<<"remain"<<remain<<endl;
-        if(remain){
-            // if(remain>6)
-            //     return {};
-            ans.push_back(remain);
+            ans[i++]++;
         }
         return ans;
     }
