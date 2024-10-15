0666aee2d5697b69b36bcfaaec4a85350febcfd6
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2024-10-16 00:44:42 +0530

    Time: 4 ms (95.99%), Space: 17.8 MB (23.66%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        sort(timePoints.begin(),timePoints.end());
+        vector<int> time;
         int ans=3600;
         int n=timePoints.size();
 
-        for(int i=0;i<n-1;i++)
+        for(int i=0;i<n;i++)
         {
-            int h1=(timePoints[i][0]-'0')*10 + (timePoints[i][1]-'0');
-            int h2=(timePoints[i+1][0]-'0')*10 + (timePoints[i+1][1]-'0');
+            int h=(timePoints[i][0]-'0')*10 + (timePoints[i][1]-'0');
+            int m=(timePoints[i][3]-'0')*10 + (timePoints[i][4]-'0');
 
-            int m1=(timePoints[i][3]-'0')*10 + (timePoints[i][4]-'0');
-            int m2=(timePoints[i+1][3]-'0')*10 + (timePoints[i+1][4]-'0');
-
-            // int diff = (h2-h1)*60 + (60-m1) + m2;
-            int t1 = h1*60 + m1;
-            int t2 = h2*60 + m2;
-            // cout<<h1<<" "<<m1<<endl;
-            // cout<<h2<<" "<<m2<<endl;
-            // cout<<"ans is: "<<ans<<endl;
-            ans = min(ans,t2-t1);
+            int t = h*60 + m;
+            time.push_back(t);
         }
+        sort(time.begin(),time.end());
+
+        for(int i=0;i<n-1;i++)
+            ans = min(ans,time[i+1]-time[i]);
         
-        int h1=(timePoints[0][0]-'0')*10 + (timePoints[0][1]-'0');
-        int m1=(timePoints[0][3]-'0')*10 + (timePoints[0][4]-'0');
-
-        int h2=(timePoints[n-1][0]-'0')*10 + (timePoints[n-1][1]-'0');
-        int m2=(timePoints[n-1][3]-'0')*10 + (timePoints[n-1][4]-'0');
-
-        int diff = (23-h2)*60 + (60-m2) + h1*60 + m1;
-        ans = min(ans,diff);
+        ans = min(ans,1440-time[n-1]+time[0]);
 
         return ans;
     }
