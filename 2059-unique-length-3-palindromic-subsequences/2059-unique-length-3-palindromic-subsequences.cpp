0599a690ee787376c1fd01522eb4f80dd6eae8db
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2025-01-04 20:14:27 +0530

    Time: 267 ms (49.7%), Space: 36.3 MB (8.28%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    private:
+    int countSubsequences(string s, char c, int l, int r)
+    {
+        unordered_set<char> st;
+        int ans=0;
+        l++;
+        while(l<r)
+        {
+            if(!st.count(s[l])){
+                ans++;
+            }
+            st.insert(s[l]);
+            l++;
+        }
+        return ans;
+    }
+public:
+    int countPalindromicSubsequence(string s) {
+        unordered_map<char,int> first, second;
+        int ans=0, n = s.size();
+
+        // find leftmost & rightmost indexes of chars
+        for(int i=0;i<n;i++)
+        {
+            char c=s[i];
+            // the char has already occured, so now update it's rightmost index
+            if(first.count(c)){
+                second[c]=i;
+            }
+            // first occurence of current char 'c'
+            else{
+                first[c]=i;
+            }
+        }
+
+        for(pair<char,int> p: first)
+        {
+            // char has freq > 1
+            if(second.count(p.first)){
+                ans+=countSubsequences(s,p.first,p.second,second[p.first]);
+            }
+        }
+
+        return ans;
+    }
+};
