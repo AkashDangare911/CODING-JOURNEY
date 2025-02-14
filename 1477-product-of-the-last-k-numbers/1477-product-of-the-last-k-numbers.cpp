8e00d77875d45abb0170bf62fb34f0c5852490b8
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2025-02-14 23:05:30 +0530

    Time: 19 ms (73.09%), Space: 78 MB (77.84%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -2,24 +2,29 @@ class ProductOfNumbers {
 public:
     vector<int> prefixProduct;
     ProductOfNumbers() {
-        prefixProduct = {};
+        prefixProduct = {1};
     }
-    
+
     void add(int num) {
-        prefixProduct.push_back(num);
+        if(num == 0)
+        // reset the array, as if we have to find product of previous 'k' elements
+        // and if any element prev 'k' elements is '0', then we don't need to calculate further,
+        //  so while calculating if we get '-ve' index, it means we have got a '0'
+            prefixProduct = {1};
+        else{
+            // maintain prefix-product
+            prefixProduct.push_back(prefixProduct.back()*num);
+        }
     }
     
     int getProduct(int k) {
-        int prod=1;
-        int lastInd = prefixProduct.size()-1;
-        while(k--)
-        {
-            if(prefixProduct[lastInd] == 0)
-                return 0;
-            prod = prod*prefixProduct[lastInd];
-            lastInd--;
-        }
-        return prod;
+        int last = prefixProduct.size()-1;
+        // this indicates that we have encountered zero somewhere at index <= k
+        if(last-k < 0)
+            return 0;
+        
+        // no zeros in left 'k' places
+        return prefixProduct.back()/prefixProduct[last-k];
     }
 };
 
