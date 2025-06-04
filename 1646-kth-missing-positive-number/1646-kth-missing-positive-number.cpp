5af90998de9207a0fd1f8d7bd9e4cc174eb6f63a
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2025-06-05 01:28:14 +0530

    Time: 0 ms (100%), Space: 13.2 MB (89.93%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        // for empty arr, missing 'k'th value would be k......{1)
-        // then if we keep adding numbers to array, it means they were not missing
-        // and we have to now search for higher k value
-        // however, if any value is greater than k, it meansthe kth missing would lie in between  
-        for(int i: arr)
-        {
-            if(i<=k)
-                k++;
-            else
-                break;
-        }
+        int n=arr.size();
+        int left=0,right=n-1;
 
-        return k;
+        while(left<=right)
+        {
+            int mid = (left+right)/2;
+
+            if((arr[mid]-(mid+1)) < k)
+                left=mid+1;
+            else
+                right=mid-1;
+        }
+        
+        // now 'right' is at the last index where missing nos < k
+        // e.g., if arr[right]=7, and right=3, then missing nos would be 1,5,6 i.e., arr[right]-(right+1)
+        // Now, we have 4 non-missing nos, then we have our ans as k+4
+        return left+k;      // as left=right+1 at the end of loop
     }
 };
