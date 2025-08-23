3c9ee243151724f3883a0faaaa0ff46dd2d29248
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2025-08-23 18:51:20 +0530

    Time: 0 ms (100%), Space: 38.2 MB (84.27%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,47 +1,38 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        int totalTime=0;
         int freq[26]={};
-        priority_queue<int> pq;
+        int maxFreq=0;
 
-        // count freq of each task
-        for(char c:tasks)
-            freq[c-'A']++;
-
-        // put frequencies in the heap
-        for(int num: freq)
-            if(num) pq.push(num);
-        
-        while(!pq.empty())
+        // build freq map & find max freq
+        for(char c: tasks)
         {
-            vector<int> temp;
-            int completed=0;
-            int cycle=n+1;
-
-            // execute cycle of n+1 tasks
-            while(cycle && !pq.empty())     // .....step 1
-            {
-                // if more tasks are available of same type, (avoid pushing 0)
-                // keep them aside to put back them in heap later
-                if(pq.top()>1)
-                    temp.push_back(pq.top()-1);
-                
-                pq.pop();
-                totalTime++;
-                cycle--;
-            }
-
-            // put back extra same type task in heap
-            for(int i:temp)
-                pq.push(i);
-
-            // it will hit if there are less than 'n' types of tasks to schedule as per step 1
-            // so in that case, we have to keep slots idle
-            if(!pq.empty())
-                totalTime+=cycle;
+            freq[c-'A']++;
+            maxFreq = max(maxFreq,freq[c-'A']);
         }
 
-        return totalTime;
+        // find elements with freq = maxFreq
+        int eleWithMaxFreq=0;
+        for(int i: freq)
+        {
+            if(i==maxFreq)
+                eleWithMaxFreq++;
+        }
+
+        // now, spread the element with maxFreq with gap of 'n' in between
+        
+        // we have maxFreq-1 slots of length n+1, A _ _ A _ _ A ==> 2*2
+        int slotLength = (maxFreq-1)*(n+1); 
+
+        // as eleWithMaxFreq will be appended after last element (after _ _ A)
+        int finalLength = slotLength+eleWithMaxFreq;
+
+        // if we have less to put in empty slots, finalLength would accomodate all elements,
+        //  including empty slots
+        // OR if we have plenty elements than empty slots, our CPU would not be idle anymore
+        // and we have to return the length of tasks as ans
+
+        int ans = max(finalLength,(int)tasks.size());
+        return ans;
     }
 };
