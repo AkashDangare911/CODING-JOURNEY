9b873054343b40bd12e33a97f42f6dce5a0ac841
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2025-09-10 20:33:11 +0530

    Time: 4 ms (17.8%), Space: 16.3 MB (12.71%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
 
-    void solve(vector<int> nums,vector<int> &temp,int index,set<vector<int>> &ans)
+    void solve(vector<int> nums,vector<int> &temp,int index,vector<vector<int>> &ans)
     {
         if(index>=nums.size()){
-            ans.insert(temp);
+            ans.push_back(temp);
             return;
         }
-
-        // exclude
-        solve(nums,temp,index+1,ans);
 
         //include
         int element = nums[index];
         temp.push_back(element);
         solve(nums,temp,index+1,ans);
         temp.pop_back();
+
+        // if we have already build ans considering the current element in recursion tree
+        // then skip the further similar elements from the tree
+        while(index+1<nums.size() && nums[index]==nums[index+1])
+            index++;
+
+         // exclude
+        solve(nums,temp,index+1,ans);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> s;
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         vector<int> temp;
         int index=0;
 
         // recursive helper function
-        solve(nums,temp,index,s);
-
-        vector<vector<int>> ans;
-        for(vector<int> v: s)
-            ans.push_back(v);
+        solve(nums,temp,index,ans);
 
         return ans;
     }
