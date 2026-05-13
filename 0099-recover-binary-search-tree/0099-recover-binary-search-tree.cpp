173a69b01b193486f6115715e8411792d3496d31
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2026-05-13 23:18:48 +0530

    Time: 0 ms (100%), Space: 60.8 MB (47.91%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,34 +11,42 @@
  */
 class Solution {
 private:
-    void getInorder(TreeNode* root, vector<int> &inorder)
-    {
-        if(!root)
-            return;
-        
-        getInorder(root->left,inorder);
-        inorder.push_back(root->val);
-        getInorder(root->right,inorder);
-    }
+    TreeNode* prev;
+    TreeNode *first;
+    // TreeNode *middle;
+    TreeNode *last;
 
-    void fillInorder(TreeNode* root, int &curInd, vector<int> &inorder)
+    void inorder(TreeNode* root)
     {
         if(!root)
             return;
         
-        fillInorder(root->left,curInd,inorder);
-        root->val=inorder[curInd++];
-        fillInorder(root->right,curInd,inorder);
+        inorder(root->left);
+
+        if(prev && prev->val > root->val)
+        {
+            // if the discrepancy happens for the first time
+            if(!first)
+            {
+                first = prev;
+                last = root;
+            }
+            else
+                last = root; // if it happens again
+        }
+
+        prev=root;
+        
+        inorder(root->right);
     }
 public:
-    // TC - O(n) + O(logn) + O(n)
-    // SC -o(N)
     void recoverTree(TreeNode* root) {
-        vector<int> inorder;
-        getInorder(root,inorder);
-        sort(inorder.begin(),inorder.end());
-        
-        int curInd=0;
-        fillInorder(root,curInd,inorder);
+        first = last = NULL;
+
+        inorder(root);
+
+        // if the nodes are not adjacent
+        if(first && last)
+            swap(first->val,last->val);
     }
 };
