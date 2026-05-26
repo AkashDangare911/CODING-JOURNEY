13eaf6c0a0d82eca7be7be427324642c999d5780
Dangare Akash <112281257+AkashDangare911@users.noreply.github.com>
2026-05-27 02:18:13 +0530

    Time: 0 ms (100%), Space: 17.3 MB (92.67%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,43 +1,28 @@
 class Solution {
 private:
-    bool checkBipartite(int ind, vector<int>& vis, vector<vector<int>>& graph)
+    bool checkBipartite(int cur, int color, vector<int>& vis, vector<vector<int>>& graph)
     {
-        // put starting point in queue and start with color - 0
-        int color=0;
-        queue<int> q;
-        vis[ind]=color;
-        q.push(ind);
+        // mark as visited
+        vis[cur]=color;
 
-        while(!q.empty())
+        bool ans = true;
+        for(int adjNode:graph[cur])
         {
-            // color the adjacent nodes with different color, hence alter the color
-            color=!color;
-            
-            int nq=q.size();
-            for(int i=0;i<nq;i++)
+            // un-visited
+            if(vis[adjNode] == -1)
             {
-                int cur=q.front();
-                q.pop();
-
-                for(int adjNode:graph[cur])
-                {
-                    // un-visited
-                    if(vis[adjNode] == -1)
-                    {
-                        vis[adjNode]=color;
-                        q.push(adjNode);
-                    }
-                    // this adjacent node is already visited by other node and has same color of current node
-                    // hence we can't color this node with same color
-                    else if(vis[adjNode] == vis[cur])
-                    {
-                        return false;
-                    }
-                }
+                // inner recursive call may return false, hence consider all outputs
+                ans = ans && checkBipartite(adjNode,!color,vis,graph);
+            }
+            // this adjacent node is already visited by other node and has same color of current node
+            // hence we can't color this node with same color
+            else if(vis[adjNode] == vis[cur])
+            {
+                return false;
             }
         }
 
-        return true;
+        return ans;
     }
 
 public:
@@ -52,7 +37,7 @@ public:
             {
                 // if any non-connected component returned false, 
                 // hence we can't paint the graph with 2 colors 
-                if(!checkBipartite(i,vis,graph))
+                if(!checkBipartite(i,0,vis,graph))
                     return false;
             }
         }
